129e155ee517d4ff6fcaf8f93400b3780955e26d
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-05-03 18:28:08 +0530

    Finish the article project

--- day-45-Beautiful_Soup/live_web_parsing.py
+++ day-45-Beautiful_Soup/live_web_parsing.py
@@ -12,6 +12,13 @@ articles = soup.find_all(name="span", class_='titleline')
 article_texts = [text.a.getText() for text in articles]
 article_links = [link.a.get('href') for link in articles]
 article_votes = [int(score.getText().split(' ')[0]) for score in soup.find_all(name="span", class_="score")]  # get the votes as an int
-print(article_texts)
-print(article_links)
-print(article_votes)
+
+# Getting the information about the most upvote article
+max_article_votes = max(article_votes)
+upvotes_index = article_votes.index(max_article_votes)
+max_article_text = article_texts[upvotes_index]
+max_article_link = article_links[upvotes_index]
+
+print(max_article_text)
+print(max_article_link)
+print(max_article_votes)
