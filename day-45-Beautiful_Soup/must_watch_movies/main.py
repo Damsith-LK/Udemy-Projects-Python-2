6d58b01e072f0dc8ebefbaaff103610bd53084fe
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-05-04 09:50:16 +0530

    Abandon 100 movies project because... Even though I try to find_all(h3) (That's where the movie titles are), I get a empty list When I looked at the solution in the course and copy pasted it, still doesn't work I assume that something has changed in the website source or something is going wrong in my side

--- day-45-Beautiful_Soup/must_watch_movies/main.py
+++ day-45-Beautiful_Soup/must_watch_movies/main.py
@@ -2,9 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 response = requests.get("https://www.empireonline.com/movies/features/best-movies-2/")
-response.raise_for_status()
 contents = response.text
 
 soup = BeautifulSoup(contents, 'html.parser')
-h3 = soup.find(name='h3')
-print(h3)
+movies = soup.find_all(name="h3", class_="title")
+print(movies)
