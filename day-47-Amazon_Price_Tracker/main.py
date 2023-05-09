c6d8d1d1309713f28406c053850a749fcfc2e7bc
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-05-09 16:03:39 +0530

    Complete day 47 - Amazon Price Tracker 1. First the program gets the price of the product (The url is given) 2. Then it checks if that price is smaller than the preferred purchase price in the code 3. If the above condition is true, then an e-mail would be sent with relevent information

--- day-47-Amazon_Price_Tracker/main.py
+++ day-47-Amazon_Price_Tracker/main.py
@@ -6,7 +6,7 @@ import lxml
 import smtplib
 import config
 
-preferred_price = 450
+PURCHASE_PRICE = 450
 URL = "https://www.amazon.com/PlayStation-5-Console-CFI-1215A01X/dp/B0BCNKKZ91/ref=sr_1_2?crid=3OJOOBDL3OVVH&keywords=ps5&qid=1683460711&sprefix=ps%2Caps%2C296&sr=8-2&th=1"
 headers = {
     "User-Agent": config.user_agent,
@@ -18,3 +18,14 @@ soup = BeautifulSoup(contents, 'lxml')
 
 price = float(soup.find(class_="a-offscreen").get_text().split("$")[1])
 print(price)
+
+if price < PURCHASE_PRICE:
+    with smtplib.SMTP("smtp.gmail.com") as conn:
+        conn.starttls()
+        conn.login(user=config.my_email, password=config.password)
+        conn.sendmail(from_addr=config.my_email, to_addrs=config.send_email, msg=
+                      "Subject:Amazon Price Alert!!!\n\n"
+                      f"PlayStation 5 Console CFI-1215A01X is now just {price}"
+                      f"\n{URL}"
+                      )
+    print("email sent.")
