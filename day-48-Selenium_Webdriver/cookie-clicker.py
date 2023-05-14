678e4f1d067409236f7087ae60691e240398c902
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-05-14 11:51:31 +0530

    Complete purchasing the most affordable expensive upgrade for every 10 seconds In previous commit message I said that the program would check every 5 seconds, but since it kept purchasing the same item over and over (because 5 seconds is too short for money to be collected),                                                                                  I extended that time to 10 seconds

--- day-48-Selenium_Webdriver/cookie-clicker.py
+++ day-48-Selenium_Webdriver/cookie-clicker.py
@@ -14,12 +14,10 @@ driver.get(URL)
 
 button = driver.find_element(By.ID, "cookie")
 
-upgrades = [
-    'buyCursor', 'buyFactory', "buyMine", "buyShipment", "buyAlchemy lab", "buyPortal",
-    "buyTime machine"
-]
+upgrades = driver.find_elements(By.CSS_SELECTOR, "#store div")
+upgrades_ids = [i.get_attribute("id") for i in upgrades]
 
-timeout = time.time() + 5  # 5 seconds
+timeout = time.time() + 10  # 10 seconds
 five_min = time.time() + 60 * 5  # 5minutes
 
 
@@ -31,13 +29,34 @@ while True:
     # You'll need to check how much money (cookies) you have against the price of each upgrade.
     if time.time() > timeout:
         current_money = int(driver.find_element(By.ID, "money").text.replace(",", ""))  # Turning into an int
-        prices = [i.text for i in reversed(driver.find_elements(By.CSS_SELECTOR, "#store b"))]
-        prices.remove("")
-        for upgrade in reversed(upgrades):
-            price = int(prices[(upgrades.index(upgrade) + 1) * -1].split("- ")[1].replace(",", ""))  # Turning into an int
-            if current_money >= price:
-                upgrade_button = driver.find_element(By.XPATH, f'//*[@id="{upgrade}"]/b')
-                upgrade_button.click()
-                time.sleep(1)
-                continue
-        timeout += 5
+        # print(current_money)
+        all_prices = driver.find_elements(By.CSS_SELECTOR, "#store b")
+        # for price in all_prices:
+        #     print(price.text)
+
+        prices_in_int = []
+        for price in all_prices:
+            element_text = price.text
+            if element_text != "":
+                cost = int(element_text.split("-")[1].strip().replace(",", ""))
+                prices_in_int.append(cost)
+
+        upgrades_dict = {}
+        for i in range(len(prices_in_int)):
+            upgrades_dict[upgrades_ids[i]] = prices_in_int[i]
+
+        # Finding the affordable upgrades
+        affordable_upgrades = {}
+        for id, cost in upgrades_dict.items():
+            if cost <= current_money:
+                affordable_upgrades[cost] = id
+
+        # Finding the maximum affordable upgrade
+        maximum_upgrade = max(affordable_upgrades.keys())
+        maximum_upgrade_id = affordable_upgrades[maximum_upgrade]
+
+        # Purchasing it
+        driver.find_element(By.ID, maximum_upgrade_id).click()
+
+        timeout += 10
+        time.sleep(0.5)  # In order to avoid crashing
