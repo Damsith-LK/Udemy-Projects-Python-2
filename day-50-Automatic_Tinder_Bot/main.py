c4730bfec81a85572c816ad1fa0f0c1f0d86a98d
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-05-20 17:54:32 +0530

    Temporarily stop working on Day 50 - Automatic Tinder Bot I can't log-in to Tinder with Google, Facebook or Phone Number because everytime I try to do that either a 'Try-again' comes or 'Verify you are a human' puzzles come up Anyways, I'm going to find out a way how to log-in with Google via Selenium later

--- day-50-Automatic_Tinder_Bot/main.py
+++ day-50-Automatic_Tinder_Bot/main.py
@@ -5,6 +5,7 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 import time
+import config
 
 chrome_driver_path = "C:\\chrome-driver\\chromedriver.exe"
 service = Service(executable_path=chrome_driver_path)
@@ -13,8 +14,19 @@ driver.maximize_window()
 
 URL = "https://tinder.com/"
 driver.get(URL)
-main_page = driver.current_window_handle
 
 driver.find_element(By.XPATH, "//*[text()='Log in']").click()  # Clicking on "Log In"
+print("Click on 'Continue with Google' Manually")
 # Can't find out a way to automatically click on "Log In with Google", so going to do it manually
-time.sleep(5)
+time.sleep(7)
+
+# After clicking on "Continue with Google":
+main_window = driver.window_handles[0]
+login_window = driver.window_handles[1]
+driver.switch_to.window(login_window)  # Switching to login window
+
+# Entering the email and "continue" button
+driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[2]/div/c-wiz/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input').send_keys(config.email)
+driver.find_element(By.XPATH, '//*[@id="identifierNext"]/div/button').click()
+
+time.sleep(20)
