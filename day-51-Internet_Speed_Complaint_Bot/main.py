fcbf9d6a6b8dc6384351ee042a6d8b331bf6cea4
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-06-10 16:45:57 +0530

    Do some corrections to the code and define tweet msgs

--- day-51-Internet_Speed_Complaint_Bot/main.py
+++ day-51-Internet_Speed_Complaint_Bot/main.py
@@ -6,9 +6,9 @@ from selenium.webdriver.chrome.service import Service
 from selenium.common.exceptions import NoSuchElementException
 import config
 
-
 CHROME_DRIVER_PATH = "C:\\chrome-driver\\chromedriver.exe"
 SPEED_TEST_URL = "https://www.speedtest.net"
+
 
 class InternetSpeedBot:
 
@@ -16,8 +16,8 @@ class InternetSpeedBot:
         service = Service(executable_path=CHROME_DRIVER_PATH)
         self.driver = Chrome(service=service)
         self.driver.maximize_window()
-        self.down = 0
-        self.up = 0
+        self.down = 50  # In mbps
+        self.up = 40
 
     def get_internet_speed(self):
         """Fetches the internet speeds from "https://www.speedtest.net" """
@@ -25,14 +25,16 @@ class InternetSpeedBot:
         self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a').click()  # Clicking on "Go" button
         time.sleep(50)  # Waiting for it to get the internet speed
         # Getting the download speed
-        self.down = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span').text
-        self.up = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
-        print(f"Download Speed: {self.down}")
-        print(f"Upload Speed: {self.up}")
+        down = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span').text
+        up = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
+        print(f"Download Speed: {down}")
+        print(f"Upload Speed: {up}")
+        return float(down), float(up)
 
     def tweet_at_provider(self):
         """Tweets the internet speeds got from the get_internet_speed() method"""
 
+        download_speed, upload_speed = self.get_internet_speed()
         # Going to the twitter log in page
         self.driver.get("https://twitter.com/i/flow/login")
         time.sleep(1)
@@ -57,6 +59,10 @@ class InternetSpeedBot:
 
         # Clicking on "Log In" button
         self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div').click()
+
+        bad_tweet = f"Hey Internet Provider, why is my internet speed {download_speed}down/{upload_speed}up when I pay" \
+                    f"for {self.down}down/{self.up}/up?"
+        good_tweet = f"Hey Internet Provider, my internet speed is as it should be. Thanks for it."
 
         ####### After logging in to Twitter #######
         # Writing the tweet (Have to wait 20 seconds to load up the page)
