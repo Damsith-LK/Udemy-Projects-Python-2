c23c2beecc65df0508a9ff0fce6bf22739725f04
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-06-17 17:33:37 +0530

    Program can now fetch the links and avoid unwanted things

--- day-53-Renting_Research/main.py
+++ day-53-Renting_Research/main.py
@@ -6,6 +6,7 @@ import requests
 import config
 
 RENTING_WEBSITE_LINK = "https://www.zillow.com/homes/for_rent/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22mapBounds%22%3A%7B%22west%22%3A-123.048563375%2C%22east%22%3A-121.96915663671875%2C%22south%22%3A37.39423627832906%2C%22north%22%3A38.02328490484952%7D%2C%22isMapVisible%22%3Afalse%2C%22filterState%22%3A%7B%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%7D"
+WEBSITE_URL = "https://www.zillow.com"
 
 
 class RentingResearch:
@@ -24,13 +25,15 @@ class RentingResearch:
         soup = BeautifulSoup(response.content, "html.parser")
 
         houses_ul = soup.find(id="grid-search-results").find(name="ul")
-        houses = houses_ul.find_all("li")
-        for house in houses:
-            link = house.findNext(name="a")
-            print(link.get_attribute_list("href")[0])
-            print(link.text)
-            print("--------------------------------------------------------------------------------------------------")
+        lst = houses_ul.find_all(name="a", attrs={"class": "property-card-link", "tabindex": "0"}, recursive=True)
 
+        for l in lst:
+            link = l["href"]  # Link to the renting house
+            # If there is something wrong with the link (if no https://zillow.com)
+            if WEBSITE_URL not in link:
+                link = WEBSITE_URL + link
+            print(link)
+            print('--------------------------------------------------------------------------------------------------')
 
     def update(self):
         """Enters the collected data into a Google form using Selenium"""
