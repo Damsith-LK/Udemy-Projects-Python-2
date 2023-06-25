ff1bff1165022f4a8513aed6a3ba8cbacff3f772
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-06-25 12:04:02 +0530

    Complete update() method It seems as if get_houses_info() method is incomplete because in the course they get information about all the houses in that page, while my program only takes information about top ones

--- day-53-Renting_Research/main.py
+++ day-53-Renting_Research/main.py
@@ -3,6 +3,10 @@
 
 from bs4 import BeautifulSoup
 import requests
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
 import config
 
 
@@ -11,8 +15,10 @@ class RentingResearch:
     def __init__(self):
         self.RENTING_WEBSITE_LINK = "https://www.zillow.com/homes/for_rent/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22mapBounds%22%3A%7B%22west%22%3A-123.048563375%2C%22east%22%3A-121.96915663671875%2C%22south%22%3A37.39423627832906%2C%22north%22%3A38.02328490484952%7D%2C%22isMapVisible%22%3Afalse%2C%22filterState%22%3A%7B%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%7D"
         self.WEBSITE_URL = "https://www.zillow.com"
+        self.FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSezQl8R8P3F726aoHg4eJp4-yjU9uYMfiiITwxZcciZ2Pfu0g/viewform?usp=sf_link"
+        self.CHROME_DRIVER_PATH = "C:\\chrome-driver\\chromedriver.exe"
 
-    def get_houses_info(self):
+    def get_houses_info(self) -> tuple:
         """Fetches relevant information about rent houses from zillow.com using BeautifulSoup
         Returns fetches items in lists"""
 
@@ -42,11 +48,32 @@ class RentingResearch:
 
         return links, prices, addresses
 
-    def update(self):
+    def update(self, house_links: list[str], house_prices: list[str], house_addresses: list[str]):
         """Enters the collected data into a Google form using Selenium"""
-        pass
+        service = Service(executable_path=self.CHROME_DRIVER_PATH)
+        driver = Chrome(service=service)
+        driver.get(self.FORM_URL)
+        time.sleep(2)
+
+        for ind in range(len(house_addresses)):
+            # Identifying the entries
+            address_entry = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+            price_entry = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+            link_entry = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+            submit_button = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div')
+            time.sleep(2)
+
+            # Entering the details
+            address_entry.send_keys(house_addresses[ind])
+            price_entry.send_keys(house_prices[ind])
+            link_entry.send_keys(house_links[ind])
+            submit_button.click()
+            time.sleep(2)
+            driver.get(self.FORM_URL)
+            time.sleep(2)
+
 
 
 rr = RentingResearch()
-rr.get_houses_info()
-rr.update()
+links_list, prices_list, addresses_list = rr.get_houses_info()
+rr.update(links_list, prices_list, addresses_list)
